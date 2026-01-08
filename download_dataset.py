58a4889cac00a0cc91831b1e971c6044380e7108
Junchen Liu <junchenliu@cs.toronto.edu>
2026-01-08 11:24:27 -0500

    update dataset downloading & env

--- download_dataset.py
+++ download_dataset.py
@@ -1,15 +1,21 @@
 import os
 
-# Set HuggingFace home to scratch directory
-os.environ["HF_HOME"] = os.path.join(os.environ.get("SCRATCH", "/scratch/junchen"), ".cache", "huggingface")
+# Set HuggingFace home to local disk which has enough space
+os.environ["HF_HOME"] = os.path.join("/datasets/DL3DV-DSO", ".cache", "huggingface")
 
 from datasets import load_dataset
+from datasets.config import HF_DATASETS_CACHE
 
-cache_dir = os.path.join(os.environ.get("SCRATCH", "/scratch/junchen"), "datasets/fineweb-edu")
+cache_dir = os.path.join("/datasets/DL3DV-DSO/fineweb-edu")
 os.makedirs(cache_dir, exist_ok=True)
 
-# load fineweb-edu with parallel processing
-dataset = load_dataset("HuggingFaceFW/fineweb-edu", name="default", num_proc=1, cache_dir=os.path.join(cache_dir, "default"))
-
-# or load a subset with roughly 100B tokens, suitable for small- or medium-sized experiments
-dataset = load_dataset("HuggingFaceFW/fineweb-edu", name="sample-100BT", num_proc=1, cache_dir=os.path.join(cache_dir, "sample-100BT"))
+# Load a subset with roughly 100B tokens, suitable for small- or medium-sized experiments
+dataset = load_dataset(
+    "HuggingFaceFW/fineweb-edu",
+    # name="default",
+    name="sample-100BT",
+    # num_proc=None,  # Disable multiprocessing to avoid subprocess crashes
+    num_proc=4,
+    cache_dir=os.path.join(cache_dir, "sample-100BT"),
+    trust_remote_code=True,
+)
